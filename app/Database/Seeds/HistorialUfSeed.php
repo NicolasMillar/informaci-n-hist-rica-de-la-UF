545bc7d6b04111be35f5a6d87551f372c4b0da3a
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-06 14:35:28 -0400

    se creo el seed que permiete popular la bd con los datos historiocos

--- app/Database/Seeds/HistorialUfSeed.php
+++ app/Database/Seeds/HistorialUfSeed.php
@@ -4,5 +4,12 @@ namespace App\Database\Seeds;
 use CodeIgniter\Database\Seeder;
 
 class HistorialUfSeed extends Seeder{
-    public function run
+
+    public function run(){
+        
+        $json = file_get_contents(APPPATH . '\Helpers\ufhistorico.json');
+        $data = json_decode($json, true);
+
+        $this->db->table('historialuf')->insertBatch($data);
+    }
 }

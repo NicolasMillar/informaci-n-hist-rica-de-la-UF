3d0d414f9daf0f0d49cd5305b95b9c497f40c7a0
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-06 17:51:06 -0400

    cambios en el seeder

--- app/Database/Seeds/HistorialUfSeed.php
+++ app/Database/Seeds/HistorialUfSeed.php
@@ -9,7 +9,15 @@ class HistorialUfSeed extends Seeder{
         
         $json = file_get_contents(APPPATH . '\Helpers\ufhistorico.json');
         $data = json_decode($json, true);
+        $filteredData = array();
+        //alamacena solamente los valores de la UF
+        foreach ($data as $element) {
+            if ($element['codigoIndicador'] == 'UF') {
+                $filteredData[] = $element;
+            }
+        }
+        
 
-        $this->db->table('historialuf')->insertBatch($data);
+        $this->db->table('historialuf')->insertBatch($filteredData);
     }
 }

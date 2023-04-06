a1bd70f086a13de7847266e1cd948002ff336453
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-06 15:06:59 -0400

    maquetacion de crud

--- app/Views/actualizar.php
+++ app/Views/actualizar.php
@@ -16,7 +16,8 @@
         <h1>Crud Historial Uf</h1>
         <div class="row">
             <div class="col-sm-12">
-                <form action="<?php echo base_url().'/crear' ?>" method="post">
+                <form action="<?php echo base_url().'/actualizar' ?>" method="post">
+                    <input type="text" name="id" id="id" hidden="">
                     <label for="nombreIndicador">Nombre Indicador</label>
                     <input type="text" name="nombreIndicador" id="nombreIndicador" class="form-control">
                     <label for="codigoIndicador">Codigo Indicador</label>
@@ -36,34 +37,13 @@
                 </form>
             </div>
         </div>
-        <hr>
-        <h2>Listado Historico del Uf</h2>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="table table-responsive">
-                    <table class="table table-hover table-bordered">
-                        <tr>
-                            <th>Nombre Indicador</th>
-                            <th>Codigo Indicador</th>
-                            <th>Unidad de Medida Indicado</th>
-                            <th>Valor Indicador</th>
-                            <th>Fecha Indicador</th>
-                            <th>Tiempo Indicador</th>
-                            <th>Origen Indicador</th>
-                            <th>Editar</th>
-                            <th>Eliminar</th>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
